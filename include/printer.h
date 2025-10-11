e5d6bd60081133637e580eb77398346c773c5cd0
joshoti <joshoti539@gmail.com>
2025-10-11 16:04:31 -0700

    update printer interface

--- include/printer.h
+++ include/printer.h
@@ -1,6 +1,13 @@
 #ifndef PRINTER_H
 #define PRINTER_H
 
+#include <pthread.h>
+
+struct LinkedList;
+struct TimedQueue;
+struct SimulationParameters;
+struct SimulationStatistics;
+
 // --- Printer structure ---
 typedef struct Printer {
     int id; // Unique identifier for the printer
@@ -10,6 +17,11 @@ typedef struct Printer {
 } Printer;
 
 // --- Utility functions ---
+/**
+ * @brief Prints printer details for debugging purposes.
+ * @param printer_id Unique identifier for the printer.
+ * @param printed_count Total number of jobs printed by this printer.
+ */
 void debug_printer(int printer_id, int printed_count);
 
 // --- Printer Thread Arguments ---
@@ -17,16 +29,20 @@ void debug_printer(int printer_id, int printed_count);
  * @brief Arguments for the printer thread.
  */
 typedef struct PrinterThreadArgs {
-    // TODO: confirm
-    pthread_mutex_t* printer_mutex;
+    // TODO: use printer_mutex?
+    // pthread_mutex_t* printer_mutex;
+    pthread_mutex_t* paper_refill_queue_mutex;
     pthread_mutex_t* job_queue_mutex;
     pthread_mutex_t* stats_mutex;
     pthread_mutex_t* simulation_state_mutex; // protects g_terminate_now
     pthread_cond_t* job_queue_not_empty_cv;
-    pthread_cond_t* printer_paper_empty_cv;
-    struct LinkedList* job_queue;
+    pthread_cond_t* refill_needed_cv;
+    struct TimedQueue* job_queue;
+    struct LinkedList* paper_refill_queue;
     struct SimulationParameters* params;
     struct SimulationStatistics* stats;
+    int* all_jobs_served;
+    int* all_jobs_arrived;
     Printer* printer;
 } PrinterThreadArgs;
 
