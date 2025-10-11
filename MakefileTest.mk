733ada73c9951927c1734f7d07be4caff3bb1b79
joshoti <joshoti539@gmail.com>
2025-10-10 21:25:46 -0700

    update test suite with job_receiver

--- MakefileTest.mk
+++ MakefileTest.mk
@@ -1,24 +1,40 @@
 # Compiler, flags, and libraries
 CC = gcc
 
-CFLAGS = -g -Wall -Iinclude -Iinclude/common
+CFLAGS = -g -Wall -Iinclude -Iinclude/common -Iexternal
 
 # --- Configuration for Executables ---
-TARGETS = test_linked_list test_preprocessing
+TARGETS = test_linked_list test_preprocessing test_job_receiver
 
 # --- Rules ---
 all: $(TARGETS)
 
 test_linked_list: test_linked_list.o linked_list.o test_utils.o
-	@echo "Compiling $@..."
 	$(CC) $(CFLAGS) -o $@ $^ 
 
 test_preprocessing: test_preprocessing.o preprocessing.o test_utils.o
-	@echo "Compiling $@..."
+	$(CC) $(CFLAGS) -o $@ $^
+
+test_job_receiver: test_job_receiver.o job_receiver.o test_utils.o preprocessing.o linked_list.o timeutils.o simulation_stats.o logger.o
 	$(CC) $(CFLAGS) -o $@ $^
 
 test_preprocessing.o: tests/test_preprocessing.c include/preprocessing.h include/common/common.h
 	$(CC) $(CFLAGS) -c tests/test_preprocessing.c
+
+test_job_receiver.o: tests/test_job_receiver.c include/job_receiver.h
+	$(CC) $(CFLAGS) -c tests/test_job_receiver.c
+
+timeutils.o: src/common/timeutils.c include/common/timeutils.h include/common/common.h
+	$(CC) $(CFLAGS) -c src/common/timeutils.c
+
+simulation_stats.o: src/simulation_stats.c include/simulation_stats.h include/common/common.h
+	$(CC) $(CFLAGS) -c src/simulation_stats.c
+
+job_receiver.o: src/job_receiver.c include/job_receiver.h include/linked_list.h include/common/common.h include/simulation_stats.h include/common/timeutils.h include/preprocessing.h include/logger.h
+	$(CC) $(CFLAGS) -c src/job_receiver.c
+
+logger.o: src/logger.c include/logger.h include/common/common.h
+	$(CC) $(CFLAGS) -c src/logger.c
 
 preprocessing.o: src/preprocessing.c include/preprocessing.h include/common/common.h
 	$(CC) $(CFLAGS) -c src/preprocessing.c
--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,5 @@
 make -f MakefileTest.mk
 ./test_linked_list
 ./test_preprocessing
+./test_job_receiver
 make -f MakefileTest.mk clean
