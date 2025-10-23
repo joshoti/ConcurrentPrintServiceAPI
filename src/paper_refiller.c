fa6bd6f838f4f4a7b41a0f9c9a1123748f9083ee
joshoti <joshoti539@gmail.com>
2025-10-23 02:09:25 -0700

    update paper_refiller to avoid deadlock and to check thread state better

--- src/paper_refiller.c
+++ src/paper_refiller.c
@@ -41,65 +41,67 @@ void* paper_refiller_thread_func(void* arg) {
     while (1) {
         pthread_mutex_lock(args->paper_refill_queue_mutex);
 
-        // Enable cancellation just before the cancellation point to avoid deadlock
-        pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
-        pthread_cond_wait(args->refill_needed_cv, args->paper_refill_queue_mutex); // wait until signaled to refill paper
-        pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+        for (;;) {
+            // Safely check shared flags
+            pthread_mutex_lock(args->simulation_state_mutex);
+            int terminate_now = g_terminate_now;
+            int are_all_jobs_served = *(args->all_jobs_served);
+            pthread_mutex_unlock(args->simulation_state_mutex);
 
-        // Safely check shared flags
-        pthread_mutex_lock(args->simulation_state_mutex);
-        int terminate_now = g_terminate_now;
-        int are_all_jobs_served = *(args->all_jobs_served);
-        pthread_mutex_unlock(args->simulation_state_mutex);
-
-        if (terminate_now) {
-            if (g_debug) printf("Paper refiller thread signaled to terminate\n");
-            pthread_cond_broadcast(args->refill_needed_cv); // wake up server threads to let them exit if needed
-            pthread_mutex_unlock(args->paper_refill_queue_mutex);
-            return NULL;
-        }
-        if (is_exit_condition_met(are_all_jobs_served)) {
-            pthread_cond_broadcast(args->refill_needed_cv); // wake up printer threads to let them exit if needed
-            pthread_mutex_unlock(args->paper_refill_queue_mutex);
-            break;
-        }
-
-        while (!list_is_empty(args->paper_refill_queue)) {
-            unsigned long refill_start_time_us = get_time_in_us();
-            
-            ListNode* elem = list_pop_left(args->paper_refill_queue);
-            Printer* printer = (Printer*)elem->data;
-            pthread_mutex_unlock(args->paper_refill_queue_mutex); // unlock while refilling
-
-            // Refill paper
-            int papers_needed = printer->capacity - printer->current_paper_count;
-            if (papers_needed <= 0) {
-                if (g_debug) printf("Debug: Paper Refiller found printer %d already full\n", printer->id);
+            if (terminate_now || is_exit_condition_met(are_all_jobs_served)) {
+                if (g_debug) printf("Paper refiller thread signaled to terminate\n");
+                pthread_cond_broadcast(args->refill_needed_cv); // wake up printer threads to let them exit if needed
+                pthread_mutex_unlock(args->paper_refill_queue_mutex);
+                goto exit_refiller;
             }
 
-            int time_to_refill_us = (unsigned long)((papers_needed / args->params->refill_rate) * 1000000);
-            int time_to_refill_ms = time_to_refill_us / 1000; // for logging and publishing
-            log_paper_refill_start(printer, papers_needed, time_to_refill_ms, refill_start_time_us);
-            
-            pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
-            usleep(time_to_refill_us);
-            pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
-            
-            unsigned long refill_end_time_us = get_time_in_us();
-            int actual_refill_time_ms = (refill_end_time_us - refill_start_time_us) / 1000;
-            log_paper_refill_end(printer, actual_refill_time_ms, refill_end_time_us);
+            if (!list_is_empty(args->paper_refill_queue)) {
+                break; // there's work
+            }
 
-            // Done refilling
-            printer->current_paper_count += papers_needed;
-            pthread_mutex_lock(args->stats_mutex);
-            args->stats->papers_refilled += papers_needed;
-            args->stats->total_refill_service_time_us += refill_end_time_us - refill_start_time_us;
-            args->stats->paper_refill_events++;
-            pthread_mutex_unlock(args->stats_mutex);
-            free(elem);
-            if (g_debug) debug_refiller(papers_needed);
+            // Enable cancellation just before the cancellation point to avoid deadlock
+            // pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+            pthread_cond_wait(args->refill_supplier_cv, args->paper_refill_queue_mutex); // wait until signaled to refill paper
+            // pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
         }
+        unsigned long refill_start_time_us = get_time_in_us();
+        ListNode* elem = list_pop_left(args->paper_refill_queue);
+        Printer* printer = (Printer*)elem->data;
+        pthread_mutex_unlock(args->paper_refill_queue_mutex); // unlock while refilling
+
+        // Refill paper
+        int papers_needed = printer->capacity - printer->current_paper_count;
+        if (papers_needed <= 0) {
+            if (g_debug) printf("Debug: Paper Refiller found printer %d already full\n", printer->id);
+        }
+
+        int time_to_refill_us = (unsigned long)((papers_needed / args->params->refill_rate) * 1000000);
+        log_paper_refill_start(printer, papers_needed, time_to_refill_us, refill_start_time_us);
+        
+        pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+        usleep(time_to_refill_us);
+        pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+        
+        unsigned long refill_end_time_us = get_time_in_us();
+        int refill_duration_us = refill_end_time_us - refill_start_time_us;
+        log_paper_refill_end(printer, refill_duration_us, refill_end_time_us);
+
+        // Done refilling: update printer state and simulation stats
+        printer->current_paper_count += papers_needed;
+        pthread_mutex_lock(args->stats_mutex);
+        args->stats->papers_refilled += papers_needed;
+        args->stats->total_refill_service_time_us += refill_end_time_us - refill_start_time_us;
+        args->stats->paper_refill_events++;
+        pthread_mutex_unlock(args->stats_mutex);
+        free(elem);
+        if (g_debug) debug_refiller(papers_needed);
+
+        // Notify waiting printers that refill is done
+        pthread_mutex_lock(args->paper_refill_queue_mutex);
+        pthread_cond_broadcast(args->refill_needed_cv);
+        pthread_mutex_unlock(args->paper_refill_queue_mutex);
     }
+exit_refiller:
     if (g_debug) printf("Paper refiller gracefully exited\n");
     return NULL;
 }
